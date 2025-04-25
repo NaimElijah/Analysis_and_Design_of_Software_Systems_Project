dd4c8024eef2e607d59356952b9eb7fa874cbade
guettarotem <guettar@post.bgu.ac.il>
2025-04-25 16:29:53 +0300

    organize files

--- dev/Util/Week.java
+++ dev/Util/Week.java
@@ -5,17 +5,23 @@ import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
 
-public record Week(int year, int week) {
+public record Week(int year, int week) implements Comparable<Week> {
     public static Week from(LocalDate date) {
         WeekFields wf = WeekFields.of(Locale.getDefault());
         return new Week(date.getYear(), date.get(wf.weekOfWeekBasedYear()));
     }
 
     public static LocalDate getNextSunday(LocalDate fromDate) {
-        // אם היום ראשון, זה נחשב השבוע הזה. אנחנו רוצים את ראשון הבא
         int daysUntilSunday = (DayOfWeek.SUNDAY.getValue() - fromDate.getDayOfWeek().getValue() + 7) % 7;
         if (daysUntilSunday == 0) daysUntilSunday = 7;
         return fromDate.plusDays(daysUntilSunday);
     }
-}
 
+    @Override
+    public int compareTo(Week other) {
+        if (this.year != other.year) {
+            return Integer.compare(this.year, other.year);
+        }
+        return Integer.compare(this.week, other.week);
+    }
+}
